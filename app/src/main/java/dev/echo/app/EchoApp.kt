003ef437303779442a6127336ae0ad5aa72e81e5
Ibrahim Khaled <Ibrahimkhaled2008@outlook.sa>
2026-09-07 09:48:32 +0300

    Remove FFmpeg initialization
    Removed FFmpeg initialization from EchoApp.

--- app/src/main/java/dev/echo/app/EchoApp.kt
+++ app/src/main/java/dev/echo/app/EchoApp.kt
@@ -2,7 +2,6 @@ package dev.echo.app
 
 import android.app.Application
 import android.util.Log
-import com.yausername.youtubedl_android.FFmpeg
 import com.yausername.youtubedl_android.YoutubeDL
 
 class EchoApp : Application() {
@@ -18,13 +17,6 @@ class EchoApp : Application() {
             Log.d("EchoApp", "YouTube-DL initialized")
         } catch (t: Throwable) {
             Log.e("EchoApp", "Failed to initialize YouTube-DL", t)
-        }
-
-        try {
-            FFmpeg.getInstance().init(this)
-            Log.d("EchoApp", "FFmpeg initialized")
-        } catch (t: Throwable) {
-            Log.e("EchoApp", "Failed to initialize FFmpeg", t)
         }
     }
 }
